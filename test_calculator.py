c0d438d53c8969c903fa458e5920cab9848824c9
syedjaferk <learnwithjafer@gmail.com>
2024-12-15 21:09:00 +0530

    feat: calculator added

---
+++ test_calculator.py
@@ -0,0 +1,31 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-2, 3), 1)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-5, -5), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(-3, 4), -12)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-10, 2), -5)
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
+
+if __name__ == "__main__":
+    unittest.main()
